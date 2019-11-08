5aa00df7953918b3fcb3ebc59200fa4109b54ab1
evuz <jesus_jgb_45@hotmail.com>
2019-11-08 22:54:14 +0100

    feat(proxy): add PORT from enviroment

--- packages/proxy/src/index.js
+++ packages/proxy/src/index.js
@@ -4,6 +4,7 @@ const proxy = require('http-proxy-middleware');
 const app = express();
 
 const PROXY_URL = process.env.proxy_url;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(
@@ -14,4 +15,9 @@ app.use(
   }),
 );
 
-app.listen(3000);
+app.listen(PORT, err => {
+  if (err) {
+    console.error(err);
+  }
+  console.log(`Listening at http://localhost:${PORT}`);
+});
