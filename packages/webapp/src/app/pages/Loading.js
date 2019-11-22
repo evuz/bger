4e95955b21e37834b8ffacfbd982e8f86eb1ab89
evuz <jesus_jgb_45@hotmail.com>
2019-11-22 01:49:38 +0100

    feat(app): Loading Screen

---
+++ packages/webapp/src/app/pages/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function LoadingPage() {
+  return (
+    <div className="Page Loading">
+      <div className="Loading__lds-circle">
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default LoadingPage;
