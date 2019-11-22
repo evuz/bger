3eefaf3140a0c5fc089b85808679bf53a860fc52
evuz <jesus_jgb_45@hotmail.com>
2019-11-22 01:50:46 +0100

    feat(app): Show Loading while get user

--- packages/webapp/src/app/App.js
+++ packages/webapp/src/app/App.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import useDomain from './hooks/useDomain';
 
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
+import Loading from './pages/Loading';
+import { UserSymbols } from '@bger/domain';
 
 function App() {
+  const [isAppInit, initApp] = useState(false);
+  const [userData, getUser] = useDomain(UserSymbols.UseCases.GetUser);
+
+  useEffect(() => {
+    setTimeout(() => {
+      getUser();
+    }, 1000);
+  }, [getUser]);
+
+  useEffect(() => {
+    const { data, error } = userData;
+    if (data || error) {
+      initApp(true);
+    }
+  }, [userData]);
+
+  if (!isAppInit) {
+    return <Loading />;
+  }
+
   return (
     <Router>
       <Switch>
